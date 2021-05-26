81482829ca4c7e7846d892a05eb455df83f56f7c
Ariye Bar-tov <ariyeb@gmail.com>
2021-05-26 18:36:21 +0300

    Add project files.

---
+++ Apartments -MVC-Course/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Apartments__MVC_Course.MvcApplication" Language="C#" %>
---
+++ Apartments -MVC-Course/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Apartments__MVC_Course.Startup))]
+namespace Apartments__MVC_Course
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
