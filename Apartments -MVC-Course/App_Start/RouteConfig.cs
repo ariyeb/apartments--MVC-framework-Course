fca740b142d8b034824413281070e174e3b5912a
Ariye Bar-tov <ariyeb@gmail.com>
2021-05-26 18:38:18 +0300

    adding map routes to routesConfig

--- Apartments -MVC-Course/App_Start/RouteConfig.cs
+++ Apartments -MVC-Course/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace Apartments__MVC_Course
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
